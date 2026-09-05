8a7f05dd2c72818d5bab912c935bf3db91e8e7f6
maximBoolBool <remezmax117@gmail.com>
2026-09-05 22:36:20 +0300

    2DTDS-1

---
+++ Assets/Scripts/Gates.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Gates : MonoBehaviour
+    {
+        [SerializeField]
+        private Gates _relatedGates;
+
+        public void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                collision.gameObject.transform.position = new(
+                    _relatedGates.transform.position.x,
+                    (float)(_relatedGates.transform.position.y-1.73),
+                    _relatedGates.transform.position.z
+                );
+            }
+        }
+    }
+}
