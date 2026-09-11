96f9638d871720efd65914499f7c8af16a4e4fb1
maximBoolBool <remezmax117@gmail.com>
2026-09-11 15:29:03 +0300

    PRT-2

---
+++ Assets/Scripts/GameStates/GlobalGameState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.GameStates
+{
+    public class GlobalGameState : MonoBehaviour
+    {
+        public GlobalGameState Instance { get; private set; }
+
+        public void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }       
+    }
+}
