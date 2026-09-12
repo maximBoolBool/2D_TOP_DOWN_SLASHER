b4d159a81bb6e59a235a9340eff224bd97797c99
maximBoolBool <remezmax117@gmail.com>
2026-09-12 14:20:28 +0300

    PRT-2

--- Assets/Scripts/Enemies/BaseEnemy.cs
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -35,8 +35,7 @@ namespace Assets.Scripts.Enemies
             Vector2 desiredDirection = (player.transform.position - transform.position).normalized;
             float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
 
-            RaycastHit2D losHit = Physics2D.Raycast(transform.position, desiredDirection, distanceToPlayer, obstacleLayer);
-            if (losHit.collider != null)
+            if (!LineOfSightHelper.IsHasLineOfSight(transform.position, desiredDirection, distanceToPlayer, obstacleLayer))
             {
                 return;
             }
---
+++ Assets/Scripts/Helpers/LineOfSightHelper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+namespace Assets.Scripts.Helpers
+{
+    public static class LineOfSightHelper
+    {
+        public static bool IsHasLineOfSight(
+            Vector3 start,
+            Vector2 direction,
+            float distance,
+            LayerMask obstacleLayer
+        )
+        {
+            var losHit = Physics2D.Raycast(start, direction, distance, obstacleLayer);
+            return losHit.collider == null;
+        }
+    }
+}
