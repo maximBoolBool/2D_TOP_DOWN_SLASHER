f2b487eb22eff7a74d4a76aefaae3b22505d5886
maximBoolBool <remezmax117@gmail.com>
2026-09-15 23:21:02 +0300

    PRT-2-2

---
+++ Assets/Scripts/Models/InteractiveWrapper.cs
@@ -0,0 +1,50 @@
+﻿using Assets.Scripts.Constants;
+using Assets.Scripts.PlayerControllers;
+using UnityEngine;
+
+namespace Assets.Scripts.Models
+{
+    public class InteractiveWrapper : MonoBehaviour
+    {
+        private bool isPlayerInRange = false;
+        private BaseInteractiveItem _interactiveItem;
+        private Animator animator;
+
+        public void Interact()
+        {
+            if (isPlayerInRange)
+            {
+                _interactiveItem.Interact();
+            }
+        }
+
+        private void Awake()
+        {
+            _interactiveItem = GetComponentInChildren<BaseInteractiveItem>();
+            animator = GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!collision.gameObject.CompareTag(TagConstants.PLAYER))
+            {
+                return;
+            }
+
+            isPlayerInRange = true;
+            // сообщаем игроку, что теперь именно этот объект — текущий интерактивный
+            collision.GetComponent<PlayerInteractController>()?.SetInteractiveWrapper(this);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (!collision.gameObject.CompareTag(TagConstants.PLAYER))
+            { 
+                return;
+            }
+
+            isPlayerInRange = false;
+            collision.GetComponent<PlayerInteractController>()?.SetInteractiveWrapper(null);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControllers/PlayerInteractController.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts.Models;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.PlayerControllers
+{
+    public class PlayerInteractController : MonoBehaviour
+    {
+        private PlayerInput _playerInput;
+        private InteractiveWrapper? _interactiveWrapper;
+
+        private void Awake()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+        }
+
+        public void SetInteractiveWrapper(InteractiveWrapper? interactiveWrapper)
+        {
+            _interactiveWrapper = interactiveWrapper;
+        }
+
+        private void OnEnable()
+        {
+            _playerInput.actions["Interact"].started += OnInteract;
+        }
+
+        private void OnDisable()
+        {
+            _playerInput.actions["Interact"].started -= OnInteract;
+
+        }
+
+        private void OnInteract(InputAction.CallbackContext context)
+        {
+            if (_interactiveWrapper != null)
+            {
+                _interactiveWrapper.Interact();
+            }
+        }
+    }
+}
